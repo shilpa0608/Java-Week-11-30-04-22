40aa6ec04031839eba11d7d3d7fccaa796c5576c
shilpashetty <shilpashetty0608@gmail.com>
2022-04-30 20:57:40 +0100

    commit message

---
+++ LeftTriangleStar.java
@@ -0,0 +1,15 @@
+package com.learning.java;
+public class LeftTriangleStar {
+    public static void main(String[] args) {
+        for (int i = 1; i <=5; i++) {
+            for (int j = i; j <=5; j++) {
+                System.out.print("  ");
+            }
+            for(int k=1;k<=i;k++){
+                System.out.print(" *");
+            }
+            System.out.println();
+        }
+    }
+}
+
